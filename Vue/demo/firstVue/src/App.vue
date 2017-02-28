e12fd9ba8978b93f8456028c739e999e23d9bc11
若水 <lufei_ying@sina.com>
2017-02-28 10:15:55 +0800

    vue

---
+++ Vue/demo/firstVue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Hi></Hi>
+  </div>
+</template>
+
+<script>
+import Hi from './components/Hi'
+export default{
+  name:'app',
+  components:{
+    Hi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ Vue/demo/firstVue/src/components/Hi.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>what is vue</div>
+</template>
+<script>
+	export default {
+	 name: 'hi',
+	 data () {
+	    return {
+	      msg: 'haha'
+	    }
+	  }
+}
+</script>
