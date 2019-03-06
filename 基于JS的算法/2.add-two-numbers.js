19cf448206ff6853777a32d09b5c63b10525dc4f
若水 <1242893065@qq.com>
2019-03-06 20:35:06 +0800

    Create 2.add-two-numbers.js

---
+++ 基于JS的算法/2.add-two-numbers.js
@@ -0,0 +1,46 @@
+leetcode 2   take example of Peng Hu
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var List = new ListNode(0);
+    var head = List;
+    var sum = 0;
+    var carry = 0;
+
+    while(l1!==null||l2!==null||sum>0){
+
+        if(l1!==null){
+            sum = sum + l1.val;
+            l1 = l1.next;
+        }
+        if(l2!==null){
+            sum = sum + l2.val;
+            l2 = l2.next;
+        }
+        if(sum>=10){
+            carry = 1;
+            sum = sum - 10;
+        }
+
+        head.next = new ListNode(sum);
+        head = head.next;
+
+        sum = carry;
+        carry = 0;
+
+    }
+
+    return List.next;
+};
+
+/*基于尾插法的链表，正好每次得到的节点和计算后的顺序是相反的*/
